1f26ac0bb0f8c5b76d064e93b2b3f5fd1b9f8e0c
JamesMeEE <wongchapat.james@gmail.com>
2026-08-18 19:21:39 +0700

    add 2 menu

--- api/config.js
+++ api/config.js
@@ -19,7 +19,7 @@ const DEFAULT_CONFIG = {
   croissants: { croissant: true, mixed_berry: true, pain_au_chocolat: true, biscoff: true, strawberry_nutella: true },
   tiramisus: { og: true, ferrero: true },
   sourdoughs: { country: true, cranberry: true, multigrain: true, darkrye: true, fig_walnut: true, olive_lemon: true, trio: true },
-  sandwiches: { avocado_shrimps: true, smoked_salmon: true, sunny_side_up: true, strawberry_blueberry: true },
+  sandwiches: { avocado_shrimps: true, smoked_salmon: true, sunny_side_up: true, strawberry_blueberry: true, tuna_melt: true, ham_cheese: true },
   extras: { cream_cheese: true, mixed_berry_jam: true, apricot_jam: true },
   options: { country: true, supper: true, cream_cheese: true, nutella: true },
   prices: {},
